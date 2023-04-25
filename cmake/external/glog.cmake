871d0fc2b39be5a728117b69960abf0ccf180f4a
gouzi <530971494@qq.com>
2023-04-25 11:14:29 +0800

    [cmake] add glog url

--- cmake/external/glog.cmake
+++ cmake/external/glog.cmake
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# https://github.com/google/glog/tree/v0.4.0
+
 include(ExternalProject)
 
 set(GLOG_PREFIX_DIR ${THIRD_PARTY_PATH}/glog)
